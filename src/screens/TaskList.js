6472397fcb8452a82ab8ec5b69a656ccfc4481b0
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-03-10 19:08:39 -0300

    Filtrar Tarefas

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -15,6 +15,7 @@ export default class TaskList extends Component{
     
     state = {
         showDoneTasks: true,
+        visibleTasks: [],
         tasks: [{
             id: Math.random(),
             desc: 'Comprar Livro de React Native',
@@ -28,8 +29,23 @@ export default class TaskList extends Component{
         },]
     }
 
+    componentDidMount = () => {
+        this.filterTasks()
+    }
+
     toggleFilter = () => {
-        this.setState({ showDoneTasks: !this.state.showDoneTasks })
+        this.setState({ showDoneTasks: !this.state.showDoneTasks }, this.filterTasks)
+    }
+
+    filterTasks = () => {
+        let visibleTasks = null
+        if(this.state.showDoneTasks){
+            visibleTasks = [...this.state.tasks]
+        } else {
+            const pending = task => task.doneAt === null
+            visibleTasks = this.state.tasks.filter(pending)
+        }
+        this.setState({ visibleTasks })
     }
 
     toggleTask = taskId => {
@@ -40,7 +56,7 @@ export default class TaskList extends Component{
             }
         })
 
-        this.setState({ tasks })
+        this.setState({ tasks }, this.filterTasks)
     }
 
     render(){
@@ -63,7 +79,7 @@ export default class TaskList extends Component{
                 </ImageBackground>
                 <View style={styles.taskList}>
                     <FlatList 
-                        data={this.state.tasks}
+                        data={this.state.visibleTasks}
                         keyExtractor={item => `${item.id}`}
                         renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask}/>}
                     />
