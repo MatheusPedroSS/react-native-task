e2d4ae8ff75ac16bb485fcc8d5bce50043e67fc2
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-03-11 11:09:28 -0300

    Tela Autenticação #02

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -2,13 +2,23 @@ import React, {Component} from 'react'
 import {
     ImageBackground,
     Text,
-    StyleSheet
+    StyleSheet,
+    View,
+    TextInput,
+    TouchableOpacity,
+    Platform
 } from 'react-native'
  
 import backgroundImg from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
 
 export default class Auth extends Component{
+
+    state = {
+        email: '',
+        password: '',
+    }
+
     render() {
         return (
             <ImageBackground 
@@ -16,6 +26,25 @@ export default class Auth extends Component{
                 style={styles.background}
             >
                 <Text style={styles.title}>Tasks</Text>
+                <View style={styles.formContainer}>
+                    <TextInput 
+                        placeholder='E-mail' 
+                        value={this.state.email}
+                        style={styles.input}
+                        onChangeText={email => this.setState({ email })}
+                    />
+                    <TextInput 
+                        placeholder='Senha' 
+                        value={this.state.password}
+                        style={styles.input}
+                        onChangeText={password => this.setState({ password })}
+                    />
+                    <TouchableOpacity >
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>Entrar</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </ImageBackground>
         )
     }
@@ -33,5 +62,26 @@ const styles = StyleSheet.create({
         color: commonStyles.colors.secondary,
         fontSize: 70,
         marginBottom: 10
+    },
+    formContainer: {
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        padding: 20,
+        width: "90%",
+    },
+    input: {
+        marginTop: 10,
+        backgroundColor: '#FFF',
+        padding: Platform.OS == 'ios' ? 15 : 10
+    },
+    button: {
+        backgroundColor: '#080',
+        marginTop: 10,
+        padding: 10,
+        alignItems: 'center'
+    },
+    buttonText: {
+        fontFamily: commonStyles.fontFamily,
+        color: '#FFF',
+        fontSize: 20
     }
 })
