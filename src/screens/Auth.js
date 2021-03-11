4ef97d748e9f891d684f13d2d5ad51dc31fc678c
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-03-11 11:19:19 -0300

    Tela Autenticação #03

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -15,8 +15,11 @@ import commonStyles from '../commonStyles'
 export default class Auth extends Component{
 
     state = {
+        name: '',
         email: '',
         password: '',
+        confirmPassword: '',
+        stageNew: true,
     }
 
     render() {
@@ -27,6 +30,18 @@ export default class Auth extends Component{
             >
                 <Text style={styles.title}>Tasks</Text>
                 <View style={styles.formContainer}>
+                    <Text style={styles.subtitle}>
+                        {this.state.stageNew ? 'Crie a sua conta' : 'Informe seus dados'}
+                    </Text>
+                    {
+                        this.state.stageNew &&
+                        <TextInput 
+                            placeholder='Nome' 
+                            value={this.state.name}
+                            style={styles.input}
+                            onChangeText={name => this.setState({ name })}
+                        />
+                    }
                     <TextInput 
                         placeholder='E-mail' 
                         value={this.state.email}
@@ -35,13 +50,26 @@ export default class Auth extends Component{
                     />
                     <TextInput 
                         placeholder='Senha' 
+                        secureTextEntry={true}
                         value={this.state.password}
                         style={styles.input}
                         onChangeText={password => this.setState({ password })}
                     />
+                    {
+                        this.state.stageNew &&
+                        <TextInput 
+                            placeholder='Confirmação de senha' 
+                            secureTextEntry={true}
+                            value={this.state.confirmPassword}
+                            style={styles.input}
+                            onChangeText={confirmPassword => this.setState({ confirmPassword })}
+                        />
+                    }
                     <TouchableOpacity >
                         <View style={styles.button}>
-                            <Text style={styles.buttonText}>Entrar</Text>
+                            <Text style={styles.buttonText}>
+                                {this.state.stageNew ? 'Registrar' : 'Entrar'}
+                            </Text>
                         </View>
                     </TouchableOpacity>
                 </View>
@@ -61,6 +89,13 @@ const styles = StyleSheet.create({
         fontFamily: commonStyles.fontFamily,
         color: commonStyles.colors.secondary,
         fontSize: 70,
+        marginBottom: 10
+    },
+    subtitle: {
+        fontFamily: commonStyles.fontFamily,
+        color: '#FFF',
+        fontSize: 20,
+        textAlign: 'center',
         marginBottom: 10
     },
     formContainer: {
