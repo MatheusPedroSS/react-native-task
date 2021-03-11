5ab434647e946ef798aaf491bace71a65499783d
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-03-11 13:28:18 -0300

    Integrando com Backend #02

--- src/common.js
+++ src/common.js
@@ -7,7 +7,7 @@ const server = Platform.OS === 'ios'
     ? 'http://localhost:3000' : 'http://10.0.2.2:3000'
 
 function showError(err) {
-    Alert.alert('Ops! Ocorreu um Problema!', `Mensagem: ${err}`)
+    Alert.alert('Ops! Ocorreu um Problema!', `Mensagem: ${err.toString()}`)
 }
 
 function showSucess(msg) {
