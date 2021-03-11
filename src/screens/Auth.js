5742e72da37ad6c31367e75ff1e410a2b47be1eb
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-03-11 17:34:17 -0300

    Autenticação ou App #02

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -8,7 +8,8 @@ import {
     Alert
 } from 'react-native'
 import axios from 'axios'
- 
+import AsyncStorage from '@react-native-community/async-storage' 
+
 import backgroundImg from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
 import AuthInput from '../components/AuthInput'
@@ -21,8 +22,8 @@ import {
 
 const initialState = {
     name: '',
-    email: 'matheus@gmail.com',
-    password: '123456',
+    email: '',
+    password: '',
     confirmPassword: '',
     stageNew: false,
 }
@@ -64,6 +65,7 @@ export default class Auth extends Component{
                 password: this.state.password
             })
 
+            AsyncStorage.setItem('userData', JSON.stringify(res.data))
             axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
             this.props.navigation.navigate('Home', res.data)
         } catch(e) {
