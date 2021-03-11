eeda3aa191ee2c14d14ece5916c7aae08c5b96fd
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-03-11 16:56:05 -0300

    Drawer Navigator #04

---
+++ src/screens/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+    ScrollView,
+} from 'react-native'
+import { 
+    DrawerItems
+} from 'react-navigation-drawer'
+
+export default props => {
+    return(
+        <ScrollView >
+            <DrawerItems {...props} />
+        </ScrollView>
+    )
+}
